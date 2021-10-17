7175551ddf87727efd6ab00811cb9ccd62eab080
David Rivas <david.rivas@pevaar.com>
2021-10-17 16:19:19 -0500

    Add project files.

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CollatzConjecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collatz conjecture");
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int steps = Steps(num);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Number of steps: " + steps);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        static int Steps(int num)
+        {
+            int steps = 0;
+            Console.WriteLine(num);
+
+            while (num > 1)
+            {
+                num = num % 2 == 0
+                    ? num / 2
+                    : 3 * num + 1;
+
+                steps++;
+                Console.WriteLine(num);
+            }
+
+            return steps;
+        }
+    }
+}
