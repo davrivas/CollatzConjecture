0d15ba4ea5e6d13c9a5e63b758d78721ddcc3eab
David Rivas <david.rivas@pevaar.com>
2021-12-11 22:02:03 -0500

    Add model creation

--- CollatzConjecture/Program.cs
+++ CollatzConjecture/Program.cs
@@ -1,37 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CollatzConjecture
 {
-    internal class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Collatz conjecture");
-            Console.WriteLine("Enter a number");
-            int num = int.Parse(Console.ReadLine());
-            int steps = Steps(num);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Number of steps: " + steps);
-            Console.ResetColor();
+            Console.WriteLine();
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                var model = GetModel(i);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Number: {0}", model.Number);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Number of steps: {0}", model.Steps.Count());
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Steps: {0}", model.StepsStr);
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
         }
 
-        static int Steps(int num)
+        static Model GetModel(int num)
         {
-            int steps = 0;
-            Console.WriteLine(num);
+            var model = new Model
+            {
+                Number = num,
+                Steps = Enumerable.Empty<int>()
+            };
 
             while (num > 1)
             {
                 num = num % 2 == 0
                     ? num / 2
                     : 3 * num + 1;
-
-                steps++;
-                Console.WriteLine(num);
+                model.Steps = model.Steps.Append(num);
             }
 
-            return steps;
+            return model;
         }
+    }
+
+    internal class Model
+    {
+        public int Number { get; set; }
+        public IEnumerable<int> Steps { get; set; }
+        public string StepsStr => string.Join(", ", Steps);
     }
 }
