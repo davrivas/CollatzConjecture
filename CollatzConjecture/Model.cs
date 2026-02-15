cc4db1d083a3bad5b93cf0f706afb9ea2fb5781f
David Rivas <davrivas1995@gmail.com>
2026-02-14 22:38:05 -0500

    Refactor Collatz logic; update to .NET 10.0
    Moved sequence logic to Model.cs and simplified Program.cs.
    Model now generates steps in constructor and uses List<int>.
    Removed old GetModel method and class wrappers.
    Updated project to target .NET 10.0.

---
+++ CollatzConjecture/Model.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CollatzConjecture
+{
+    public class Model
+    {
+        public int Number { get; set; }
+        public List<int> Steps { get; set; } = [];
+        public string StepsStr => string.Join(", ", Steps);
+
+        public Model(int num)
+        {
+            Number = num;
+
+            while (num > 1)
+            {
+                num = num % 2 == 0
+                    ? num / 2
+                    : 3 * num + 1;
+                Steps.Add(num);
+            }
+        }
+    }
+}
--- CollatzConjecture/Program.cs
+++ CollatzConjecture/Program.cs
@@ -1,56 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using CollatzConjecture;
+using System;
 
-namespace CollatzConjecture
+Console.WriteLine("Collatz conjecture");
+Console.WriteLine();
+
+for (int i = 1; i <= 1000; i++)
 {
-    static class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Collatz conjecture");
-            Console.WriteLine();
-
-            for (int i = 1; i <= 1000; i++)
-            {
-                var model = GetModel(i);
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Number: {0}", model.Number);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Number of steps: {0}", model.Steps.Count());
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Steps: {0}", model.StepsStr);
-                Console.ResetColor();
-                Console.WriteLine();
-            }
-
-            Console.ReadKey();
-        }
-
-        static Model GetModel(int num)
-        {
-            var model = new Model
-            {
-                Number = num,
-                Steps = Enumerable.Empty<int>()
-            };
-
-            while (num > 1)
-            {
-                num = num % 2 == 0
-                    ? num / 2
-                    : 3 * num + 1;
-                model.Steps = model.Steps.Append(num);
-            }
-
-            return model;
-        }
-    }
-
-    internal class Model
-    {
-        public int Number { get; set; }
-        public IEnumerable<int> Steps { get; set; }
-        public string StepsStr => string.Join(", ", Steps);
-    }
+    var model = new Model(i);
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Number: {0}", model.Number);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Number of steps: {0}", model.Steps.Count);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Steps: {0}", model.StepsStr);
+    Console.ResetColor();
+    Console.WriteLine();
 }
